c41a00bd7c187f7a689d802a6eacf1abf5606bea
Martinez Calvet <pumz@gft.com>
2016-01-31 18:01:24 +0100

    Random and non-repeated answer buttons

--- app/main-page.ts
+++ app/main-page.ts
@@ -18,18 +18,18 @@ interface Task {
     selector: 'main',
     template: `
 <StackLayout orientation='vertical'>
-    <Button text="New Game"></Button>
-    
-    <GridLayout columns="*,*,*,*,*" rows="100">
+    <Button text="New Game" (tap)="startGame()" [visibility]="gameStarted ? 'collapse' : 'visible'"></Button>
+
+    <GridLayout columns="*,*,*,*,*" rows="100" [visibility]="gameStarted ? 'visible' : 'collapse'">
         <Label class="task-label" [text]="task.arg1" col="0"></Label>
         <Label class="task-label" [text]="task.operand" col="1"></Label>
         <Label class="task-label" [text]="task.arg2" col="2"></Label>
         <Label class="task-label" text="=" col="3"></Label>
         <Label class="task-label" text="?" col="4"></Label>
     </GridLayout>
-    
-    <StackLayout>
-        <Button *ngFor="#answer of task.answers; #i = index" 
+
+    <StackLayout [visibility]="gameStarted ? 'visible' : 'collapse'">
+        <Button *ngFor="#answer of task.answers; #i = index"
             [text]="answer" class="answer"
             (tap)="onAnswer(i)"></Button>
     </StackLayout>
@@ -39,7 +39,7 @@ interface Task {
 })
 export class MainPage {
     public task: Task;
-
+    public gameStarted:boolean = false;
     constructor() {
         this.nextTask();
     }
@@ -52,14 +52,32 @@ export class MainPage {
             arg1: arg1,
             arg2: arg2,
             operand: operand,
-            answers: [this.getResult(arg1, arg2, operand),
-                      this.getResult(this.getArg(), this.getArg(), operand),
-                      this.getResult(this.getArg(), this.getArg(), operand),
-                      this.getResult(this.getArg(), this.getArg(), operand)],
+            answers: this.getAnswers(arg1, arg2, operand),
             rightAnswerIndex: 2
         }
     }
-    
+
+    private getAnswers(arg1, arg2, operand){
+        var pos:number = Math.floor(Math.random() * 4),
+            result:Array<number> = [];
+
+        for (var i = 0; i < 4; i++){
+            var x = this.getArg(),
+                y = this.getArg(),
+                correct = this.getResult(arg1, arg2, operand);
+
+            // Controlling not to repeat results
+            if (correct !== this.getResult(x, y, operand) && !(result.indexOf(correct) > -1)) {
+                (i===pos) ? result.push(correct) :
+                            result.push(this.getResult(x, y, operand));
+            } else {
+                i -= 1;
+            }
+        }
+        
+        return result;
+    }
+
     private getResult(arg1, arg2, operand) {
         switch(operand) {
             case '+':
@@ -72,7 +90,7 @@ export class MainPage {
                 return arg1 * arg2;
                 break;
             case '/':
-                return arg1 / arg2;
+                return ((((arg1 / arg2) * 100 ) | 0 ) / 100 );
                 break;
         }
     }
@@ -80,15 +98,19 @@ export class MainPage {
     private getArg(): number {
         return Math.floor((Math.random() * 10) + 1);
     }
-    
+
     private getOperation(): string{
         var i = Math.floor((Math.random() * operands.length))
         return operands[i];
     }
-    
+
     public onAnswer(index){
         console.log("answer index: " + index);
         this.nextTask();
     }
 
+    public startGame(){
+        this.gameStarted = true;
+        //counter should start here
+    }
 }
